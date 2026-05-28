451732d6a7487c8637229f6be76842e910060db9
nanfeng <nanfengovo@gmail.com>
2026-05-28 09:01:23 +0800

    feat 首页修改

--- RCS_Vue/src/layouts/modules/global-footer/index.vue
+++ RCS_Vue/src/layouts/modules/global-footer/index.vue
@@ -6,8 +6,8 @@ defineOptions({
 
 <template>
   <DarkModeContainer class="h-full flex-center">
-    <a href="https://github.com/soybeanjs/soybean-admin/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
-      Copyright MIT © 2021 Soybean
+    <a  target="_blank" rel="noopener noreferrer">
+      Copyright MIT © 2026 RCS
     </a>
   </DarkModeContainer>
 </template>
